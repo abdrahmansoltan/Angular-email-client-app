d027ceff3c56a3b4d98f74c02095189606b9efb7
Abdelrahman Soltan <asoltan@instabug.com>
2024-04-11 19:53:15 +0200

    refactor(Auth): ♻️ move http requests to AuthService

--- src/app/Auth/validators/unique-username.ts
+++ src/app/Auth/validators/unique-username.ts
@@ -1,36 +1,32 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AsyncValidator, FormControl } from '@angular/forms';
 import { catchError, map, of } from 'rxjs';
+import { AuthService } from '../auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UniqueUsername implements AsyncValidator {
-  constructor(private http: HttpClient) {} // 👈 inject the HttpClient
+  constructor(private authService: AuthService) {} // inject the AuthService service
 
   // Arrow function to bind the context of 'this'
   validate = (control: FormControl): any => {
     const { value } = control;
 
-    return this.http
-      .post<any>('https://api.angular-email.com/auth/username', {
-        username: value,
+    return this.authService.usernameAvailable(value).pipe(
+      map((value) => {
+        if (value.available) {
+          return null;
+        }
+        return { nonUniqueUsername: true };
+      }),
+      catchError((err) => {
+        if (err.error?.username) {
+          return of({ nonUniqueUsername: true });
+        } else {
+          return of({ noConnection: true });
+        }
       })
-      .pipe(
-        map((value) => {
-          if (value.available) {
-            return null;
-          }
-          return { nonUniqueUsername: true };
-        }),
-        catchError((err) => {
-          if (err.error?.username) {
-            return of({ nonUniqueUsername: true });
-          } else {
-            return of({ noConnection: true });
-          }
-        })
-      );
+    );
   };
 }
