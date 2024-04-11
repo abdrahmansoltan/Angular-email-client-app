17defb58253cce69c46e90c156fabef93c2056af
Abdelrahman Soltan <asoltan@instabug.com>
2024-04-11 16:32:37 +0200

    feat(Auth): 🛂 Create signup form with validators

--- src/app/Auth/signup/signup.component.ts
+++ src/app/Auth/signup/signup.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrl: './signup.component.css',
 })
 export class SignupComponent {
+  authForm = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+      Validators.pattern('^[a-zA-Z0-9]*$'), // letters and numbers
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(20),
+    ]),
+    passwordConfirmation: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(20),
+    ]),
+  });
 
+  onSubmit() {
+    console.log(this.authForm.value);
+  }
 }
