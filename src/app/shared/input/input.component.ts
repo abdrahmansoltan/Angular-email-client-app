b8c3f96be87d60fc2fbb8620eca92ba4e10b6985
Abdelrahman Soltan <asoltan@instabug.com>
2024-04-12 17:39:00 +0200

    refactor(Auth): ♻️ create common Input component with validation

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrl: './input.component.css'
+  styleUrl: './input.component.css',
 })
 export class InputComponent {
+  @Input() label: string = '';
+  @Input() type: string = 'text';
+  @Input() placeholder: string = '';
+  @Input() control: FormControl = new FormControl('');
 
+  showErrors(): boolean {
+    const { dirty, touched, errors } = this.control;
+    return Boolean(dirty && touched && errors);
+  }
 }
