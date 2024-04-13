1993976ed6e044e7d25b8aaff2188de2471c3454
Abdelrahman Soltan <asoltan@instabug.com>
2024-04-13 15:28:19 +0200

    feat(Inbox): ✨ generate initial inbox components and services

---
+++ src/app/inbox/email-show/email-show.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-email-show',
+  templateUrl: './email-show.component.html',
+  styleUrl: './email-show.component.css'
+})
+export class EmailShowComponent {
+
+}
