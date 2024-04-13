4478179d7ffb4b5c151cbd929bb6a41f6f445562
Abdelrahman Soltan <asoltan@instabug.com>
2024-04-13 10:06:58 +0200

    feat(Auth): 🛂 create app header with authentication links

--- src/app/Auth/auth.service.ts
+++ src/app/Auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, tap } from 'rxjs';
 
 interface UsernameAvailableResponse {
   available: boolean;
@@ -20,6 +21,7 @@ interface SignupResponse {
 })
 export class AuthService {
   rootUrl = 'https://api.angular-email.com';
+  signedin$ = new BehaviorSubject(false); // BehaviorSubject is a type of Observable that stores the current value and emits it to new subscribers
 
   constructor(private http: HttpClient) {} // inject the HttpClient service
 
@@ -33,9 +35,24 @@ export class AuthService {
   }
 
   signup(credentials: SignupCredentials) {
-    return this.http.post<SignupResponse>(
-      `${this.rootUrl}/auth/signup`,
-      credentials
+    return this.http
+      .post<SignupResponse>(`${this.rootUrl}/auth/signup`, credentials, {
+        withCredentials: true, // send cookies along with the request
+      })
+      .pipe(
+        tap(() => {
+          this.signedin$.next(true); // emit a new value to all subscribers that the user is signed in
+        })
+      );
+  }
+
+  checkAuth() {
+    return this.http.get(`${this.rootUrl}/auth/signedin`).pipe(
+      tap((response) => {
+        if (response === true) {
+          this.signedin$.next(true);
+        }
+      })
     );
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { AuthService } from './Auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'emailClient2';
+  signedin$: BehaviorSubject<boolean>;
+
+  constructor(private authService: AuthService) {
+    this.signedin$ = this.authService.signedin$; // set the signedin$ BehaviorSubject to the signedin$ BehaviorSubject from the AuthService
+  }
+
+  ngOnInit() {
+    this.authService.checkAuth().subscribe(() => {}); // call the checkAuth method from the AuthService
+  }
 }
