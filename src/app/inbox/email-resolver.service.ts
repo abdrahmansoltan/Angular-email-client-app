b30e9d7b657e2d2e0c50bd1e6c1d54c494d882a2
Abdelrahman Soltan <asoltan@instabug.com>
2024-04-15 00:14:17 +0200

    Handle invalid email id error

--- src/app/inbox/email-resolver.service.ts
+++ src/app/inbox/email-resolver.service.ts
@@ -3,8 +3,10 @@ import {
   ActivatedRouteSnapshot,
   MaybeAsync,
   Resolve,
+  Router,
   RouterStateSnapshot,
 } from '@angular/router';
+import { EMPTY, catchError } from 'rxjs';
 import { Email } from './email';
 import { EmailService } from './email.service';
 
@@ -12,13 +14,18 @@ import { EmailService } from './email.service';
   providedIn: 'root',
 })
 export class EmailResolverService implements Resolve<Email> {
-  constructor(private emailService: EmailService) {}
+  constructor(private emailService: EmailService, private router: Router) {}
 
   resolve(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): MaybeAsync<Email> {
     const { id } = route.params;
-    return this.emailService.getEmail(id);
+    return this.emailService.getEmail(id).pipe(
+      catchError(() => {
+        this.router.navigateByUrl('/inbox/not-found');
+        return EMPTY;
+      })
+    );
   }
 }
