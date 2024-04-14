975ab4a87c05b7a2cbbe5d3391192b029434fb90
Abdelrahman Soltan <asoltan@instabug.com>
2024-04-14 22:08:33 +0200

    feat(Inbox): ✨ Add emails http methods in email service

--- src/app/inbox/email-index/email-index.component.ts
+++ src/app/inbox/email-index/email-index.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { EmailService, EmailSummary } from '../email.service';
 
 @Component({
   selector: 'app-email-index',
   templateUrl: './email-index.component.html',
-  styleUrl: './email-index.component.css'
+  styleUrl: './email-index.component.css',
 })
 export class EmailIndexComponent {
+  emails: EmailSummary[] = [];
 
+  constructor(private emailService: EmailService) {}
+
+  ngOnInit() {
+    this.emailService.getEmails().subscribe((emails) => {
+      this.emails = emails;
+    });
+  }
 }
--- src/app/inbox/email.service.ts
+++ src/app/inbox/email.service.ts
@@ -1,9 +1,43 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+export interface EmailSummary {
+  id: string;
+  subject: string;
+  from: string;
+}
+
+export interface Email {
+  id: string;
+  subject: string;
+  text: string;
+  to: string;
+  from: string;
+}
+
+export interface EmailPayload {
+  subject: string;
+  text: string;
+  to: string;
+}
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmailService {
+  rootUrl = 'https://api.angular-email.com';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getEmails() {
+    return this.http.get<EmailSummary[]>(`${this.rootUrl}/emails`);
+  }
+
+  getEmail(id: string) {
+    return this.http.get<Email>(`${this.rootUrl}/emails/${id}`);
+  }
+
+  sendEmail(email: EmailPayload) {
+    return this.http.post(`${this.rootUrl}/emails`, email);
+  }
 }
